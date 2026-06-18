1103e9e9b22e06ff88c90e7bdbfb9751a856599f
mastercoms <mastercoms@tuta.io>
2026-06-18 06:47:24 -0400

    fix not actually updating pyinstaller

--- install_pyinstaller_inner.bat
+++ install_pyinstaller_inner.bat
@@ -2,8 +2,8 @@
 git submodule update --init
 cd pyinstaller
 setlocal
-cd bootloader
-python ./waf all
-cd ..
-pip install .
+set PYINSTALLER_COMPILE_BOOTLOADER=1
+python -m pip uninstall -y PyInstaller
+python -m pip cache remove PyInstaller
+python -m pip install --verbose --no-binary=PyInstaller PyInstaller
 cd ..
